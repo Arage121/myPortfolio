04655126bd9cca36d12bd654f1dedd9f6d775484
Arage121 <rajayush746@gmail.com>
2023-05-02 20:37:50 +0530

    updated

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -39,7 +39,7 @@ const links = [
             Resume <BsFillPersonLinesFill size={30}/>
             </> 
         ),
-        href:'/resume.pdf',
+        href:'/latest_resume.pdf',
         style:'rounded-br-md',
         download:true,
     }
